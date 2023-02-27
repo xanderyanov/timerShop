b7fb15242675350d5eccf95e252f96941ef5e311
xanderyanov <xanderyanov@gmail.com>
2023-02-27 14:39:59 +0400

    1

---
+++ TimerSln/TimerStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using static TimerStore.BaseController;
+
+namespace TimerStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+
+        public ViewSettingsClass ViewSettings { get; set; }
+    }
+}
